e00d9b50547fa37d066624b0140b5b3bb62edb0e
pratikshaa-07 <pratikshashetty125@gmail.com>
2025-09-18 16:56:03 +0530

    input monitor

--- src/apb_input_monitor.sv
+++ src/apb_input_monitor.sv
@@ -1,20 +1,18 @@
 `include "defines.svh"
 
-class apb_output_monitor extends uvm_monitor #(apb_sequence_item);
+class apb_input_monitor extends uvm_monitor #(apb_sequence_item);
   `uvm_component_utils(apb_monitor)
 
   virtual apb_if.MON vif;
   apb_sequence_item seq_item;
   
   uvm_analysis_port #(apb_sequence_item) item_collected_port;   //port for coverage
-
-//new    
+    
   function new (string name, uvm_component parent);
     super.new(name, parent);
     item_collected_port = new("item_collected_port", this);
   endfunction
 
-//build phase
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
 
@@ -28,11 +26,11 @@ class apb_output_monitor extends uvm_monitor #(apb_sequence_item);
     repeat(4)
     @(posedge vif.PCLK);
     begin
-        seq_item=apb_sequence_item ::type_id::create("seq_item",this);
-        
-//capturing the signals 
-        seq_item.apb_read_data_out=vif.apb_read_data_out;
-        seq_item.PSLVERR=vif.PSLVERR;
+    seq_item=apb_sequence_item ::type_id::create("seq_item",this);    
+    //capturing the signals
+    seq_item.apb_write_paddr=vif.apb_write_paddr;    
+    seq_item.apb_read_paddr=vif.apb_read_paddr; 
+    seq_item.apb_write_data=vif.apb_write_data;  
     end
   endtask
   
