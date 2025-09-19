6c3ff184ff3b6c456181edfe8029b4050e98683b
Aruna Abirami V J <arunaabirami@feserver.mirafra.com>
2025-09-19 10:20:36 +0530

    updated subscriber and scoreboard with port names

--- src/apb_environment.sv
+++ src/apb_environment.sv
@@ -1,4 +1,3 @@
-
 class apb_environment extends uvm_env;
   apb_active_agent apb_agent_1;
   apb_passive_agent apb_agent_2;
@@ -10,7 +9,7 @@ class apb_environment extends uvm_env;
   function new(string name, uvm_component parent);
     super.new(name, parent);
   endfunction
-
+  
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
     apb_agent_1 = apb_active_agent::type_id::create("apb_agent_1", this);
@@ -18,12 +17,13 @@ class apb_environment extends uvm_env;
     apb_scoreboard_1 = apb_scoreboard::type_id::create("apb_scoreboard_1", this);
     apb_coverage_1 = apb_coverage::type_id::create("apb_coverage_1", this);
   endfunction
- 
-  function void connect_phase(uvm_phase phase);  
-    apb_agent_1.mon_in.ap.connect(apb_scoreboard_1.mon1_imp);
-    apb_agent_2.mon_out.ap.connect(apb_scoreboard_1.mon2_imp);
-    apb_agent_1.mon_in.ap.connect(apb_coverage_1.aport_mon1);
-    apb_agent_2.mon_out.ap.connect(apb_coverage_1.aport_mon2);
-  endfunction
 
+  function void connect_phase(uvm_phase phase);  
+    apb_agent_1.active_mon.item_collected_port.connect(apb_scoreboard_1.apb_input_scb_port);
+    apb_agent_2.mon_out.ap.connect(apb_scoreboard_1.apb_output_scb_port);
+    apb_agent_1.mon_in.ap.connect(apb_coverage_1.aport_ip_mon);
+    apb_agent_2.mon_out.ap.connect(apb_coverage_1.aport_out_mon);
+  endfunction
+ 
 endclass
+ 
--- src/apb_subscriber.sv
+++ src/apb_subscriber.sv
@@ -3,38 +3,36 @@
 
 class apb_coverage extends uvm_component;
   `uvm_component_utils(apb_coverage)
-  uvm_analysis_imp_act_mon_cg #(apb_sequence_item, apb_coverage) aport_mon1;
-  uvm_analysis_imp_pass_mon_cg #(apb_sequence_item, apb_coverage) aport_mon2;
-  apb_sequence_item mon1_trans, mon2_trans;
-  real mon1_cov, mon2_cov;
+  uvm_analysis_imp_act_mon_cg #(apb_sequence_item, apb_coverage) aport_ip_mon;
+  uvm_analysis_imp_pass_mon_cg #(apb_sequence_item, apb_coverage) aport_out_mon;
+  apb_sequence_item ip_mon_trans, out_mon_trans;
+  real ip_mon_cov, out_mon_cov;
   
   virtual apb_if vif;
   
-  function void build_phase(uvm_phase phase);
-    super.build_phase(phase);
-    if (!uvm_config_db#(virtual alu_interface)::get(this, "", "vif", vif)) begin
-      `uvm_info(get_type_name(), "vif not set in config_db for coverage yet — will try again later", UVM_LOW)
-      `uvm_fatal(get_type_name(), "vif not set for alu_coverage");
-    end
-  endfunction
+  covergroup active_mon_cov;   
+    TRANSFER : coverpoint ip_mon_trans.transfer{ bins transfer_bin[]  = {0,1}; }
+    READ_WRITE: coverpoint ip_mon_trans.READ_WRITE{ bins read_write_bin[]  = {0,1}; }
+    WRITE_ADDRESS: coverpoint ip_mon_trans.apb_write_paddr{ bins write_address_bin[]  = {[0:255]}; }
+    READ_ADDRESS: coverpoint ip_mon_trans.apb_read_paddr{ bins read_address_bin[]  = {[0:255]}; }
+    WRITE_DATA : coverpoint ip_mon_trans.apb_write_data{ bins write_data_bin[]  = {[0:127]}; }
     
-  
-  covergroup active_mon_cov;
-
+    TRANSFER_X_READ_WRITE : cross TRANSFER , READ_WRITE;
+    
   endgroup
   
   covergroup pass_monitor_cov;
-    
-  endgroup
-  
-  
+    READ_DATA : coverpoint out_mon_trans.apb_read_data_out{ bins read_data_bin[]  = {[0:255]}; }
+    SLAVE_ERR : coverpoint out_mon_trans.PSLVERR{ bins pslverr_bin[]  = {{0,1}; }
+                                                 
+  endgroup 
   
   function new(string name = "", uvm_component parent);
     super.new(name, parent);
     pass_monitor_cov = new;
     active_mon_cov = new;
-    aport_mon1=new("aport_mon1", this);
-    aport_mon2 = new("aport_mon2", this);
+    aport_ip_mon = new("aport_ip_mon", this);
+    aport_out_mon = new("aport_out_mon", this);
   endfunction
   
   function void write_act_mon_cg(apb_sequence_item t);
@@ -42,26 +40,25 @@ class apb_coverage extends uvm_component;
       `uvm_warning(get_type_name(), "Coverage got NULL transaction, skipping");
       return;
     end
-    mon1_trans = t;
+    ip_mon_trans = t;
     active_mon_cov.sample();
   endfunction
   
   function void write_pass_mon_cg(apb_sequence_item t);
-    mon2_trans = t;
+    out_mon_trans = t;
     pass_monitor_cov.sample();
-   
   endfunction
   
   function void extract_phase(uvm_phase phase);
     super.extract_phase(phase);
-    mon1_cov = active_mon_cov.get_coverage();
-    mon2_cov = pass_monitor_cov.get_coverage();
+    ip_mon_cov = active_mon_cov.get_coverage();
+    out_mon_cov = pass_monitor_cov.get_coverage();
   endfunction
 
   function void report_phase(uvm_phase phase);
     super.report_phase(phase);
-    `uvm_info(get_type_name(), $sformatf("[input] Coverage ------> %0.2f%%,", mon1_cov), UVM_LOW);
-    `uvm_info(get_type_name(), $sformatf("[output] Coverage ------> %0.2f%%", mon2_cov), UVM_LOW);
+    `uvm_info(get_type_name(), $sformatf("[input] Coverage ------> %0.2f%%,", ip_mon_cov), UVM_LOW);
+    `uvm_info(get_type_name(), $sformatf("[output] Coverage ------> %0.2f%%", out_mon_cov), UVM_LOW);
   endfunction
 
 endclass
